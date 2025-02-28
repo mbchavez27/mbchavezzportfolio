00b0dd066aecfc4aa774507b28ca49c11abe8ca2
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-02-28 21:49:56 +0800

    fix: setting up

---
+++ src/layout/layout.tsx
@@ -0,0 +1,8 @@
+import Home from "../pages/home";
+export default function Layout() {
+  return (
+    <>
+      <Home></Home>
+    </>
+  );
+}
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="bg-[#151515] min-h-screen">
+        <h1 className="text-[#BFBFBF] font-bold">@mbchavezz</h1>
+      </div>
+    </>
+  );
+}
