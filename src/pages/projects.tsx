b6648fdf63ceace5984113fc618f849b5c6cf224
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-03-01 02:00:08 +0800

    feat: add projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -10,12 +10,14 @@ export default function Projects() {
   return (
     <>
       <div className="bg-[#0a0a0a] min-h-screen text-[#BFBFBF]">
-        <div className="flex justify-center items-center flex-col space-y-8 min-h-screen pb-48">
+        <div className="flex justify-center items-center flex-col space-y-8 min-h-screen pb-48 transition duration-200 ease-in-out">
           <div className="text-6xl my-30">Projects</div>
           <Carousel className="w-screen h-auto">
             <CarouselContent className="space-x-8">
-              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-                <div className="bg-black border-2 p-24 rounded-lg">test</div>
+              <CarouselItem className="md:basis-1/2 ">
+                <div className="bg-black border-2 px-36 py-48 rounded-lg">
+                  test
+                </div>
               </CarouselItem>
               <CarouselItem className="md:basis-1/2 lg:basis-1/3">
                 <div className="bg-black border-2 p-24 rounded-lg">test</div>
@@ -25,7 +27,6 @@ export default function Projects() {
               </CarouselItem>
             </CarouselContent>
             <CarouselPrevious />
-            <CarouselNext />
           </Carousel>
         </div>
       </div>
