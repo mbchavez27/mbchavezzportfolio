fcd9b41901a6d953342b78f79169030bfc041573
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-03-10 10:02:15 +0800

    feat: add view resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+'use client'
+
 const Home = () => {
-  return <></>
+  return (
+    <>
+      <div className="flex flex-col items-end px-30 py-16 space-y-12">
+        <div>
+          <p className="font-bold italic text-6xl/16 w-4xl text-right">
+            I'm a currently DLSU student studying BS Computer Science Major in
+            Software Tech and a Senior Front End Engineer at La Salle Computer
+            Society
+          </p>
+        </div>
+        <div className="space-x-6">
+          <button className="text-2xl border-2 py-2 px-4 rounded-xl border-[#e93a3c] text-[#e93a3c] hover:bg-[#e93a3c] hover:text-white duration-200 transition ease-in-out">
+            Contact Me
+          </button>
+          <button
+            className="text-2xl border-2 py-2 px-4 rounded-xl border-[#e93a3c] text-[#e93a3c] hover:bg-[#e93a3c] hover:text-white duration-200 transition ease-in-out"
+            onClick={() => window.open('/resume.pdf', '_blank')}
+          >
+            View Resume
+          </button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Home
---
+++ src/app/resume/resume.tsx
@@ -0,0 +1,14 @@
+const Resume = () => {
+  return (
+    <>
+      <iframe
+        src="/resume.pdf"
+        width="100%"
+        height="800px"
+        style={{ border: 'none' }}
+      />
+    </>
+  )
+}
+
+export default Resume
