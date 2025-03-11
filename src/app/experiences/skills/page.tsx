a25ca2dac2029bfa25923ed3a90bf8c41be35cc8
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-03-12 02:22:44 +0800

    feat: skills

--- src/app/experiences/skills/page.tsx
+++ src/app/experiences/skills/page.tsx
@@ -1,5 +1,17 @@
-"use client";
-import { AnimatePresence, motion } from "motion/react";
+'use client'
+import { AnimatePresence, motion } from 'motion/react'
+import {
+  SiTypescript,
+  SiJavascript,
+  SiPython,
+  SiC,
+  SiMysql,
+  SiReact,
+} from 'react-icons/si'
+import { FaJava, FaNode } from 'react-icons/fa'
+import { IoLogoFigma } from 'react-icons/io5'
+import { RiTailwindCssFill, RiNextjsFill } from 'react-icons/ri'
+import { DiMongodb } from 'react-icons/di'
 
 const Skills = () => {
   return (
@@ -12,12 +24,41 @@ const Skills = () => {
           transition={{ duration: 1, ease: [0, 0.71, 0.2, 1.01] }}
         >
           <div className="py-3">
-            <h1 className="font-bold text-2xl">Programming Languages</h1>
+            <h1 className="font-bold text-3xl">Programming Languages</h1>
+            <div className="p-6 space-x-3 flex">
+              <SiTypescript className="size-10"></SiTypescript>
+              <SiJavascript className="size-10"></SiJavascript>
+              <SiPython className="size-10"></SiPython>
+              <SiC className="size-10"></SiC>
+              <FaJava className="size-10"></FaJava>
+            </div>
+          </div>
+          <div className="py-3">
+            <h1 className="font-bold text-3xl">Frameworks and Tools</h1>
+            <div className="p-6 space-x-3 flex">
+              <SiReact className="size-10"></SiReact>
+              <RiNextjsFill className="size-10"></RiNextjsFill>
+              <FaNode className="size-10"></FaNode>
+            </div>
+          </div>
+          <div className="py-3">
+            <h1 className="font-bold text-3xl">Design Tools</h1>
+            <div className="p-6 space-x-3 flex">
+              <IoLogoFigma className="size-10" />
+              <RiTailwindCssFill className="size-10"></RiTailwindCssFill>
+            </div>
+          </div>
+          <div className="py-3">
+            <h1 className="font-bold text-3xl">Database</h1>
+            <div className="p-6 space-x-3 flex">
+              <SiMysql className="size-10" />
+              <DiMongodb className="size-10"></DiMongodb>
+            </div>
           </div>
         </motion.div>
       </AnimatePresence>
     </>
-  );
-};
+  )
+}
 
-export default Skills;
+export default Skills
